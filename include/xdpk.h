6a5d6c1592c5ad9c5fd0af8b88c9f942fa87a027
Sirio Balmelli <sirio@b-ad.ch>
2018-05-12 14:17:32 +0200

    rough structure

--- include/xdpk.h
+++ include/xdpk.h
@@ -1,6 +1,18 @@
 #ifndef xdpk_h_
 #define xdpk_h_
 
+#include <unistd.h>
+#include <stdint.h>
 
+#define XDPK_FRAME_SZ (4096 * 3) /* larger than 9k jumbo frame, multiple of 4k pagesize */
+#define XDPK_FRAME_CNT 1024 /* default number of frames in buffer */
+
+struct xdpk_sock {
+	int fd;
+	int ifidx;
+	char *ifname;
+	size_t umem_cnt;
+	uint8_t umem[];
+};
 
 #endif /* xdpk_h_ */
--- src/xdpk.c
+++ src/xdpk.c
@@ -1,8 +1,57 @@
+#include <stdlib.h>
 #include <unistd.h>
+
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+
 #include <zed_dbg.h>
 #include <xdpk.h>
 
+
+/*	xdpk_free()
+ */
+void xdpk_sock_free(struct xdpk_sock *sk)
+{
+	if (!sk)
+		return;
+	if (sk->fd != -1)
+		close(sk->fd);
+	free(sk);
+}
+
+/*	xdpk_open()
+ * open an interface
+ */
+struct xdpk_sock *xdpk_sock_new(const char *ifname)
+{
+	struct xdpk_sock *ret = NULL;
+	size_t alloc = sizeof(struct xdpk_sock) + (XDPK_FRAME_SZ * XDPK_FRAME_CNT);
+	Z_die_if(!(
+		ret = calloc(alloc, 1)
+		), "alloc size %zu", alloc);
+	ret->fd = -1;	
+	Z_die_if((
+		ret->fd = socket(PF_XDP, SOCK_RAW, 0)
+		) == -1, "");
+
+	return ret;
+out:
+	xdpk_sock_free(ret);
+	return NULL;
+}
+
+
+/*	main()
+ */
 int main(int argc, char **argv)
 {
+	struct xdpk_sock *sk = NULL;
+
+	Z_die_if(!(
+		sk = xdpk_sock_new(argv[1])
+		), "");
+
+out:
+	xdpk_sock_free(sk);
 	return 0;
 }
