46fe12197d5e3cde9b9c04ae2c4178b9a3463e7a
Sirio Balmelli <sirio@b-ad.ch>
2018-08-05 16:29:35 +0200

    add metadata, packaging and debugging

--- default.nix
+++ default.nix
@@ -21,7 +21,16 @@ with import <nixpkgs> { inherit system; };
 
 stdenv.mkDerivation rec {
   name = "xdpacket";
+  version = "0.0.2";
   outputs = [ "out" ];
+
+  meta = with stdenv.lib; {
+    description = "the eXtremely Direct Packet handler";
+    homepage = https://siriobalmelli.github.io/xdpacket/;
+    license = licenses.gpl2;
+    platforms = platforms.unix;
+    maintainers = [ "https://github.com/siriobalmelli" ];
+  };
 
   buildInputs = [
     clang
@@ -49,6 +58,9 @@ stdenv.mkDerivation rec {
     + " -Ddep_type=${dep_type}";
 
   configurePhase = ''
+      echo "------------------ build env --------------------------"
+      env
+      echo "-------------------------------------------------------"
       echo "flags: $mFlags"
       echo "prefix: $out"
       CC=${compiler} meson --prefix=$out build $mFlags
@@ -59,4 +71,28 @@ stdenv.mkDerivation rec {
   doCheck = true;
   checkPhase = "ninja test";
   installPhase = "ninja install";
+
+  # Build packages outside $out then move them in: fpm seems to ignore
+  #+	the '-x' flag that we need to avoid packaging packages inside packages
+  fixupPhase = ''
+      mkdir temp
+      for pk in "deb" "rpm" "tar" "zip"; do
+          if ! fpm -f -t $pk -s dir -p temp/ -n $name -v $version \
+              --description "${meta.description}" \
+              --license "${meta.license.spdxId}" \
+              --url "${meta.homepage}" \
+              --maintainer "${builtins.head meta.maintainers}" \
+              "$out/=/"
+          then
+              echo "ERROR (non-fatal): could not build $pk package" >&2
+          fi
+      done
+      mkdir -p $out/var/cache/packages
+      mv -fv temp/* $out/var/cache/packages/
+  '';
+
+  # Allow YouCompleteMe and other tooling to see into the byzantine
+  #+	labyrinth of library includes.
+  # TODO: this string manipulation ought to be done in Nix.
+  shellHook=''export CPATH=$(echo $NIX_CFLAGS_COMPILE | sed "s/ \?-isystem /:/g")'';
 }
