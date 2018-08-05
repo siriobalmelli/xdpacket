73400586ced7f516b697b1abfd802f6f3a473668
Sirio Balmelli <sirio@b-ad.ch>
2018-08-05 10:13:29 +0200

    add test shell; work on Nix and Meson dependencies

---
+++ test/field_test.c
@@ -0,0 +1,13 @@
+/*	field_test.c
+ * Verify all invariant bitfield and hashing manipulation math
+ * (c) 2018 Sirio Balmelli
+ */
+
+#include <stdio.h>
+#include <zed_dbg.h>
+
+int main()
+{
+	return 0;
+}
+
